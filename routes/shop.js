1caee8c5e931598a920c7cc2c1c2b13d44b0e1eb
amanpro30 <aman.k17@iiits.in>
2019-12-27 16:29:06 +0530

    Made major modifications

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,19 @@
+const path = require('path');
+
 const express = require('express');
-const productController = require('../controllers/products');
+
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
+router.get('/', shopController.getIndex);
 
-router.get('/', productController.getProducts);
+router.get('/products', shopController.getProducts);
 
-module.exports = router
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
